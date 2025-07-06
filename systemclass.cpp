162ad56ab8e99bb26ece36570e5b8eb8df6997cc
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2025-07-06 14:51:58 +0530

    classes

---
+++ systemclass.cpp
@@ -0,0 +1,6 @@
+#include "systemclass.h"
+
+SystemClass::SystemClass(){
+	m_Input = 0;
+	m_Application = 0;
+}
---
+++ systemclass.h
@@ -0,0 +1,43 @@
+#ifndef _SYSTEMCLASS_H_
+#define _SYSTEMCLASS_H_
+
+// pre-process
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+
+#include "inputclass.h"
+#include "applicationclass.h"
+
+class SystemClass
+{
+public:
+	SystemClass();
+	SystemClass(const SystemClass&);
+	~SystemClass();
+
+	bool Intialize();
+	void Shutdown();
+	void Run();
+
+	LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+	bool Frame();
+	void InitializeWindows(int&, int&);
+	void ShutdownWindows();
+
+private:
+	LPCWSTR m_applicationName;
+	HINSTANCE m_histance;
+	HWND m_hwnd;
+
+	InputClass* m_Input;
+	ApplicationClass* m_Application;
+};
+
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+static SystemClass* ApplicationHandle = 0;
+
+#endif
